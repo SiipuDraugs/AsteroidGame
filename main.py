4f35729a4b4d876d29eb1072f06e29b75c936cda
SiipuDraugs <martinsfreimanis5@gmail.com>
2026-04-06 14:51:15 +0300

    added FPS counter

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ from logger import log_state
 
 def main():
     pygame.init()
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        color = "black"
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) /1000
+        
 
 
 if __name__ == "__main__":
